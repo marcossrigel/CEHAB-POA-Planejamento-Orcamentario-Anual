0a816e6a87dc15c57918276f59af8f9b767b21b4
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-23 20:03:19 -0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!-- http://localhost/poa/login.php -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>POA - Home</title>
+  
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+
+  <div class="content">
+    <div class="content-info">
+      <h1>Olá você chegou até aqui!</h1>
+    </div>
+
+  </div>
+
+</body>
+</html>
